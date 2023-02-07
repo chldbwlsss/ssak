ec5f34fb193c4a096d6d782cf6f0ecfcfb5f0ff4
youjin <developer_choi@naver.com>
2023-02-08 01:16:56 +0900

    spring security config and h2 config

---
+++ src/main/java/choi/ssak/config/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package choi.ssak.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                    .authorizeHttpRequests((authz) -> authz
+                            .antMatchers("/h2/**").permitAll()
+                            .anyRequest().authenticated()
+                    )
+                    .csrf().disable()
+                    .headers().frameOptions().disable()
+                    .and()
+                    .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
