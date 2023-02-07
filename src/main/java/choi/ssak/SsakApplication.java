2815747dde7d8a9568a47e5203d465b25d06ebdf
youjin <developer_choi@naver.com>
2023-02-07 23:42:33 +0900

    start ssak

---
+++ src/main/java/choi/ssak/SsakApplication.java
@@ -0,0 +1,13 @@
+package choi.ssak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsakApplication.class, args);
+	}
+
+}
